41fd4c4d0ad8aeff59de8f55a241365fc0b25b4b
Mary Mousa <mmous102@uottawa.ca>
2021-05-28 10:44:52 -0400

    lab2

---
+++ Module2/scripts/groceries.js
@@ -0,0 +1,105 @@
+	 
+
+const selectedRestrictions = new Set()
+
+var products = [
+	{
+		name: "Brocoli",
+		LactoseFree: true,
+		NutFree: true,
+		isOrganic:true,
+		price: 1.99
+	},
+	{
+		name: "Bread",
+		LactoseFree: false,
+		NutFree: false,
+		isOrganic:true,
+		price: 2.35
+	},
+	{
+		name: "Salmon",
+		LactoseFree: true,
+		NutFree: true,
+		isOrganic:true,
+		price: 10.00
+	},
+	{
+		name: "Apple",
+		LactoseFree: true,
+		NutFree: true,
+		isOrganic:false,
+		price: 1.79
+	},
+	{
+		name: "Milk",
+		LactoseFree: false,
+		NutFree: true,
+		isOrganic:true,
+		price: 8.50
+	},
+	{
+		name: "Almonds",
+		LactoseFree: true,
+		NutFree: false,
+		isOrganic:true,
+		price: 2.00
+	},
+	{
+		name: "Rice",
+		LactoseFree: true,
+		NutFree: true,
+		isOrganic:false,
+		price: 13.00
+	},
+	{
+		name: "Honey",
+		LactoseFree: true,
+		NutFree: false,
+		isOrganic:false,
+		price: 5.99
+	},
+	{
+		name: "Avacado",
+		LactoseFree: true,
+		NutFree: true,
+		isOrganic:false,
+		price: 3.99
+	},
+	{
+		name: "Strawberry",
+		LactoseFree: true,
+		NutFree: true,
+		isOrganic:true,
+		price: 2.30
+	}
+	
+];
+	
+
+function restrictListProducts(prods) {
+	let filteredProducts = [];
+	for (let i = 0; i < prods.length; i += 1) {
+	  if ((selectedRestrictions.has("Lactose-Free")) && (prods[i].LactoseFree == false)){
+		continue;
+	  }
+	  if ((selectedRestrictions.has("Nut-Free")) && (prods[i].NutFree == false)){
+		continue;
+	  }
+	  if ((selectedRestrictions.has("Organic")) && (prods[i].isOrganic == false)){
+		continue;
+	  }
+	  filteredProducts.push(prods[i]);
+	}
+	return filteredProducts;
+  }
+
+function getTotalPrice(chosenProducts) {
+	totalPrice = 0;
+	for (let i=0; i<products.length; i+=1) {
+		if (chosenProducts.indexOf(products[i].name) > -1){
+			totalPrice += products[i].price;
+		}
+	}
+	return totalPrice;
+}
---
+++ Module2/scripts/main.js
@@ -0,0 +1,94 @@
+
+const allItems = restrictListProducts(products)
+renderProductList(allItems)
+
+
+function openInfo(evt, tabName) {
+
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " active";
+
+}
+	
+
+function populateListProductChoices(slct1) {
+    var s1 = document.getElementById(slct1);
+    var s2 = document.getElementById('displayProduct');
+	
+    s2.innerHTML = "";
+	
+	if (selectedRestrictions.has(s1.value)) {
+		selectedRestrictions.delete(s1.value)
+	  } else {
+		selectedRestrictions.add(s1.value)
+	  }
+
+    var optionArray = restrictListProducts(products, s1.value);
+
+
+	renderProductList(optionArray)
+}
+	
+
+function selectedItems(){
+	
+	var ele = document.getElementsByName("product");
+	var chosenProducts = [];
+	
+	var c = document.getElementById('displayCart');
+	c.innerHTML = "";
+	
+	var para = document.createElement("P");
+	para.innerHTML = "You selected : ";
+	para.appendChild(document.createElement("br"));
+	for (i = 0; i < ele.length; i++) { 
+		if (ele[i].checked) {
+			para.appendChild(document.createTextNode(ele[i].value));
+			para.appendChild(document.createElement("br"));
+			chosenProducts.push(ele[i].value);
+		}
+	}
+		
+	c.appendChild(para);
+	c.appendChild(document.createTextNode("Total Price is " + getTotalPrice(chosenProducts)));
+		
+}
+
+function priceFilter(a,b){
+	return a.price > b.price ? 1:-1;
+}
+
+function renderProductList(optionArray){
+	var s2 = document.getElementById('displayProduct');
+	const sortedOptionArray = optionArray.sort(priceFilter);
+	for (i = 0; i < sortedOptionArray.length; i++) {
+  
+	  var productName = sortedOptionArray[i].name;
+	  var productPrice = sortedOptionArray[i].price;
+
+	  var checkbox = document.createElement("input");
+	  checkbox.type = "checkbox";
+	  checkbox.name = "product";
+	  checkbox.value = productName;
+	  s2.appendChild(checkbox);
+  
+	  formattedPrice = (Math.round(productPrice * 100) / 100).toFixed(2);
+  
+	  var label = document.createElement('label')
+	  label.htmlFor = productName;
+	  label.appendChild(document.createTextNode(`${productName} - $${formattedPrice}`));
+	  s2.appendChild(label);
+  
+	  s2.appendChild(document.createElement("br"));
+	}
+  }
