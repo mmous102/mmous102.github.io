109bf88e8db16f61f84fa8f1c3c285b1085bba21
Mary Mousa <mmous102@uottawa.ca>
2021-06-24 12:43:47 -0230

    mod

--- Module5/scripts/services.js
+++ Module5/scripts/services.js
@@ -3,7 +3,6 @@ var unavailableDates = ["06/29/2020","07/07/2020","07/10/2020"];
 const setDateFormat = "mm/dd/yy";
 
 function disableDates(date) {
-    // Sunday is Day 0, disable all Sundays
     if (date.getDay() === 0)
         return [false];
     var string = jQuery.datepicker.formatDate(setDateFormat, date);
@@ -61,11 +60,17 @@ $(document).ready(function() {
 
   
   
-
- $( function() {
-    $( "#datepicker" ).datepicker();
-   
-  } );
+$( "#dateInput" ).datepicker(
+        {
+            dateFormat: setDateFormat,
+            // no calendar before June 1rst 2020
+            minDate: new Date('06/01/2020'),
+            maxDate: '+4M',
+            // used to disable some dates
+            beforeShowDay: $.datepicker.noWeekends,
+            beforeShowDay: disableDates
+        }
+    );
 
   $("#debit").on("mouseenter", function() {
     $("#debit").addClass("showInput");
