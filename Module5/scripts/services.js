ff9a00cf183411ea24faba6faca21822fb077f46
Mary Mousa <mmous102@uottawa.ca>
2021-06-24 13:05:00 -0230

    mod

--- Module5/scripts/services.js
+++ Module5/scripts/services.js
@@ -2,7 +2,6 @@ var unavailableDates = ["06/29/2020", "07/07/2020", "07/10/2020"];
 const setDateFormat = "mm/dd/yy";
 
 function disableDates(date) {
-  // Sunday is Day 0, disable all Sundays
   if ((date.getDay() === 1 || date.getDay() === 5) && $("#doctors option:selected").text() === "Dr. Anna")
     return [false];
 
@@ -38,7 +37,11 @@ function validateCard(cardNum) {
   }
 }
 
-
+function confirme(){
+  if (validatePhone() && validateCard()){
+    return true;
+  }
+}
 $(document).ready(function() {
 
  
@@ -63,15 +66,17 @@ $(document).ready(function() {
     }
   });
 
-  
+  $("#submit").on("change", function() {
+    if (confirme("submit")) {
+      alert("Success");
+    }
+  });
   
 $( "#dateInput" ).datepicker(
         {
             dateFormat: setDateFormat,
-            // no calendar before June 1rst 2020
             minDate: new Date('06/01/2020'),
             maxDate: '+4M',
-            // used to disable some dates
             beforeShowDay: $.datepicker.noWeekends,
             beforeShowDay: disableDates
         }
